730758aa5ac4490a4fc2525673c0d76864c61baf
ARADHANA02219 <122155639+ARADHANA02219@users.noreply.github.com>
2023-10-28 16:34:09 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+const Footer = () => {
+  return (
+    <div class="button">  
+    <Button >esc</Button>
+  <Button >
+   10
+  </Button>
+  <Button >
+   50
+  </Button>
+  <Button >
+   80
+  </Button>
+  <Button >
+   100
+  </Button></div>
+    
+    
+  )
+}
+
+
+export default Footer
